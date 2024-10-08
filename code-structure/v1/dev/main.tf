a5944b6bf09576e09d0809cfcdc666a7ab9fdec4
Samir Prakash <Samir.Prakash@gmail.com>
2024-10-08 14:31:50 -0500

    added main.tf and extracted variables to variables.tf

---
+++ code-structure/v1/dev/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "spixrnc-vpc" {
+  cidr_block = var.cidr_block
+
+  enable_dns_support   = false
+  enable_dns_hostnames = false
+
+  tags = {
+    Name = "spixrnc-${var.environment}-vpc"
+  }
+}
